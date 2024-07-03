30d3c4d20af0f2c8ff1c66ddb4eff2ae4d811b0f
Martín Ezequiel del Valle <med.delvalle@gmail.com>
2024-07-03 16:31:11 -0300

    Update database.py

--- database.py
+++ database.py
@@ -5,4 +5,13 @@ database=mysql.connector.connect (
     password='',
     database='python'
 )
-
+"""
+Para ejecutar la base de datos en Pythonanywhere se reemplaza por el siguiente código:
+import mysql.connector
+database=mysql.connector.connect (
+    host='mdvalle81.mysql.pythonanywhere-services.com',
+    user='mdvalle81',
+    password='integrador',
+    database='mdvalle81$default'
+)
+"""
